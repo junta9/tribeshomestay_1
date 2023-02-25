c789bc7b21fb34c03fd2cd787a3d4bf10abd8716
Chou VANG <lesolo91@hotmail.com>
2023-02-25 20:07:08 +0100

    ajout

---
+++ src/Form/CustomRegisterType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CustomRegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('email', EmailType::class, [
+            'label' => 'E-mail',
+            'required' => true,
+            'attr' => [
+                'required' => 'required',
+                'class' => 'inputBox'
+            ]
+        ])
+        ->add('password', RepeatedType::class, [
+            'type' => PasswordType::class,
+            'invalid_message' => 'Les mots de passe doivent correspondre.',
+            'required' => true,
+            'first_options' => [
+                'label' => 'Mot de passe',
+                'attr' => [
+                    'class' => 'inputBox'
+                ]
+            ],
+            'second_options' => [
+                'label' => 'Répéter le mot de passe',
+                'attr' => [
+                    'class' => 'inputBox'
+                ]
+            ]
+        ])
+        ->add('submit', SubmitType::class, [
+            'label' => 'Envoyer',
+            'attr' => [
+                'class' => 'btn btn-dark'
+            ]
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
